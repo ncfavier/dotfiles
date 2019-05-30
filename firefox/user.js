e3103555460a8583cbbea84d0a9e8175f55c425b
Naïm Favier <fnaim42@gmail.com>
2019-05-30 12:55:08 +0200

    Shipping not included.

--- firefox/user.js
+++ firefox/user.js
@@ -6,7 +6,7 @@ user_pref('browser.newtabpage.enhanced', false);
 user_pref('browser.onboarding.enabled', false);
 user_pref('browser.startup.homepage', 'about:home');
 user_pref('browser.tabs.warnOnClose', false);
-user_pref('browser.uiCustomization.state', '{"placements":{"widget-overflow-fixed-list":[],"PersonalToolbar":["personal-bookmarks"],"nav-bar":["back-button","forward-button","stop-reload-button","home-button","urlbar-container","downloads-button","bookmarks-menu-button"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"toolbar-menubar":["menubar-items"]},"seen":["developer-button","ublock0_raymondhill_net-browser-action"],"dirtyAreaCache":["PersonalToolbar","nav-bar","TabsToolbar","toolbar-menubar"],"currentVersion":14,"newElementCount":4}');
+user_pref('browser.uiCustomization.state', '{"placements":{"widget-overflow-fixed-list":[],"nav-bar":["back-button","forward-button","stop-reload-button","home-button","urlbar-container","downloads-button","bookmarks-menu-button"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["personal-bookmarks"]},"seen":["developer-button","ublock0_raymondhill_net-browser-action"],"dirtyAreaCache":["PersonalToolbar","nav-bar","TabsToolbar","toolbar-menubar"],"currentVersion":16,"newElementCount":5}');
 user_pref('browser.urlbar.clickSelectsAll', true);
 user_pref('browser.urlbar.doubleClickSelectsAll', false);
 user_pref('browser.urlbar.maxRichResults', '5');
